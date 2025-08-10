209ef73f4466c13fec355fd7ad722fee1dc8b706
Auca Maillot <aucacoyan@gmail.com>
2025-08-10 09:31:23 -0300

    :bug: fix unnecesary feature flag

--- espanso/src/exit_code.rs
+++ espanso/src/exit_code.rs
@@ -46,7 +46,6 @@ pub const PACKAGE_LIST_FAILED: i32 = 4;
 pub const PACKAGE_UPDATE_FAILED: i32 = 5;
 pub const PACKAGE_UPDATE_PARTIAL_FAILURE: i32 = 6;
 
-#[cfg(target_os = "macos")]
 pub const UNEXPECTED_RUN_AS_ROOT: i32 = 42;
 
 use crate::error_eprintln;
