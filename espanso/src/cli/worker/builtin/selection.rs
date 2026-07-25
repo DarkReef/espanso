d2381aab519b113d485df8e33f201913d748cbf9
Dark Rift <imaganate.dark@gmail.com>
2026-07-25 11:54:32 +0600

    feat: add selected text hotkey builtin

---
+++ espanso/src/cli/worker/builtin/selection.rs
@@ -0,0 +1,36 @@
+/*
+ * This file is part of espanso.
+ *
+ * Copyright (C) 2019-2021 Federico Terzi
+ *
+ * espanso is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * espanso is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with espanso.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+use espanso_engine::event::EventType;
+
+use crate::cli::worker::builtin::generate_next_builtin_id;
+
+use super::BuiltInMatch;
+
+pub const DEFAULT_SELECTION_MATCH_SHORTCUT: &str = "CTRL+ALT+M";
+
+pub fn create_match_execute_selection() -> BuiltInMatch {
+    BuiltInMatch {
+        id: generate_next_builtin_id(),
+        label: "Execute match from selected text",
+        triggers: Vec::new(),
+        hotkey: Some(DEFAULT_SELECTION_MATCH_SHORTCUT.to_owned()),
+        action: |_| EventType::SelectionMatchRequested,
+    }
+}
