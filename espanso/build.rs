b456e7dba20bbbb0722947c5c8ec5beac19e9123
Dark Rift <imaganate.dark@gmail.com>
2026-07-28 17:32:26 +0600

    fix: embed icon without duplicate Windows version resource

--- espanso/build.rs
+++ espanso/build.rs
@@ -1,8 +1,28 @@
 #[cfg(windows)]
 fn main() {
-    println!("cargo:rerun-if-changed=../assets/respanso.ico");
+    use std::{env, fs, path::PathBuf};
+
+    let manifest_dir = PathBuf::from(
+        env::var_os("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR is not set"),
+    );
+    let icon_path = manifest_dir.join("../assets/respanso.ico");
+    let output_dir = PathBuf::from(env::var_os("OUT_DIR").expect("OUT_DIR is not set"));
+    let resource_file = output_dir.join("respanso-icon.rc");
+    let icon_resource_path = icon_path.to_string_lossy().replace('\\', "/");
+
+    println!("cargo:rerun-if-changed={}", icon_path.display());
+    fs::write(
+        &resource_file,
+        format!("1 ICON \"{icon_resource_path}\"\n"),
+    )
+    .expect("failed to create the rEspanso icon resource file");
+
     let mut resource = winres::WindowsResource::new();
-    resource.set_icon("../assets/respanso.ico");
+    resource.set_resource_file(
+        resource_file
+            .to_str()
+            .expect("rEspanso icon resource path is not valid UTF-8"),
+    );
     resource
         .compile()
         .expect("failed to embed the rEspanso Windows icon");
