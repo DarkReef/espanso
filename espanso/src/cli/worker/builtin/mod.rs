a0680f06ccc0237df1a8153fe204c477d95bd822
Dark Rift <imaganate.dark@gmail.com>
2026-09-19 01:49:50 +0300

    fix(hotkeys): register fallback selection match

--- espanso/src/cli/worker/builtin/mod.rs
+++ espanso/src/cli/worker/builtin/mod.rs
@@ -62,6 +62,7 @@ pub fn get_builtin_matches(config: &dyn Config) -> Vec<BuiltInMatch> {
         process::create_match_exit(),
         process::create_match_restart(),
         selection::create_match_execute_selection(),
+        selection::create_match_execute_selection_fallback(),
         selection::create_match_ai_rewrite(),
     ];
 
